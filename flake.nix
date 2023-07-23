5af2a894bfd535a30beb1146b43b97e67fca33dd
John Wang <jzw2@illinois.edu>
2023-07-23 15:09:21 +0200

    Add build
    That feeling when it takes you a week to make hello world work.
    Sad face.

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "A simple Nix Flake for running a Prolog script";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.scryer.url = "github:jzw2/scryer-prolog";
+
+  outputs = { self, nixpkgs, scryer}:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-darwin;
+      prolog = scryer.defaultPackage.x86_64-darwin;
+    in
+    {
+      defaultPackage.x86_64-darwin = pkgs.writeShellScriptBin "piscis" ''
+        #!/bin/sh
+        exec ${prolog}/bin/scryer-prolog main.pl -g main
+      '';
+    };
+}
