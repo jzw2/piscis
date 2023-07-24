7f704db86f895ec330fc267b92ff6a6e4bfc82d1
John Wang <jzw2@illinois.edu>
2023-07-24 13:42:55 +0200

    Basic repl

--- main.pl
+++ main.pl
@@ -1,5 +1,26 @@
 
 :- use_module(library(format)).
+:- use_module(library(pio)).
+:- use_module(library(dcgs)).
+:- use_module(library(os)).
+
+:- initialization(main).
+
+as --> [].
+as --> [a], as.
+
+xs(said_hi) --> "hi" .
+xs(quit) --> "quit" .
+
+%% main :- phrase_from_stream(xs(Thing), user_input), format("~w", [Thing]).
+%%
+%%
+
+main :- format("Welcome to the shell~n", []), repl.
 
 
-main :- format("hello world!", []).
+repl :- write('><> '),
+        read(X),
+        (X = exit , write('OK Bye!'), halt;
+        format("You entreed command ~w, executing", [X]),  (shell(X) -> format("Sucesss!", []), repl ;
+        write("Oops, that didn't work, try again. "), repl) ).
