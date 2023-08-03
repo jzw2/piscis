d3c5491d7af9b085f585858f7fe57a8c58de22d1
John Wang <jzw2@illinois.edu>
2023-08-03 16:58:12 +0200

    Cleanup

--- main.pl
+++ main.pl
@@ -19,38 +19,13 @@ lower([Char | Rest]) --> [Char], { char_type(Char, lower) }, lower(Rest).
 main :- format("Welcome to the shell~n", []), repl.
 
 
-red :-  format("\x1b\[1;31m  hi", []).
+handle_list(List) :-
+    shell(List) -> format("~nSuccess", []) ;
+     format("Encountered a failure with ~s", [List]).
 
-up(N) :- format("\x1b\[~wA", [N]) .
-
-
-clear :- format("\x1b\[2J", []).
-
-home :- format("\x1b\[H", []).
-
-getplace :- format("\x1b\[6n", []).
-
-save :- format("\x1b\[s", []).
-
-load :- format("\x1b\[u", []).
-
-backspace :- format("\x1b\H", []).
-
-backspace2 :- format("~b", []).
-
-report :- format("\x1b\[6n ok what next", []), read(X), write(X).
-
-repl :-
-    %% format("~n><> ",[]),
-        %%  current_input(InputStream),
-        get_single_char(C),
-
-        (C = '\x1b\H' -> format("We are done", []);
-                C = '\b' -> write('\b'), repl;
-        format("~w", [C]), repl).
-
-        %% get_line_to_chars(InputStream, RawInput, []),
-        %% append(NoNewLine, "\n", RawInput),
-        %% (NoNewLine = "exit" , write('OK Bye!'), halt;
-        %% format("You entreed command ~s, executing ~n", [NoNewLine]),  (shell(NoNewLine) -> format("Sucesss!", []), repl ;
-        %% write('Oops, that didn\'t work, try again. '), repl) ).
+repl :- repeat,
+        format("~n><> ",[]),
+         get_line_to_chars(user_input, RawInput, []),
+         append(NoNewLine, "\n", RawInput),
+         handle_list(NoNewLine),
+         false.
